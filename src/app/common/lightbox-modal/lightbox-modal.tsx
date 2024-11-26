e6d4931c33a14e4af20771fcd0245b5c609963bf
eaglelizard <whitedtd@gmail.com>
2024-11-26 04:29:29 -0700

    revert mui/base update due to https://github.com/mui/base-ui/issues/875

--- src/app/common/lightbox-modal/lightbox-modal.tsx
+++ src/app/common/lightbox-modal/lightbox-modal.tsx
@@ -1,7 +1,7 @@
 
 import './lightbox-modal.scss';
 import React from 'react';
-import { Modal } from '@mui/base/Modal';
+import ModalUnstyled from '@mui/base/ModalUnstyled';
 
 type LightboxModalProps = {
   open: boolean;
@@ -12,7 +12,7 @@ type LightboxModalProps = {
 
 export function LightboxModal(props: LightboxModalProps) {
   return (
-    <Modal
+    <ModalUnstyled
       className="lightbox-modal"
       slots={{
         backdrop: LightboxModalBackdrop,
@@ -26,7 +26,7 @@ export function LightboxModal(props: LightboxModalProps) {
           props.children
         }
       </div>
-    </Modal>
+    </ModalUnstyled>
   );
 
   function handleOnClose() {
